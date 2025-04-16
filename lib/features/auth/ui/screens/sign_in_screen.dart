7257e55192a8c501a537222fb4a51354df4a345e
mahin <mdmahinurrahmannifad@gmail.com>
2025-04-17 00:39:32 +0600

    API connection

--- lib/features/auth/ui/screens/sign_in_screen.dart
+++ lib/features/auth/ui/screens/sign_in_screen.dart
@@ -7,6 +7,7 @@ import 'package:ecommerce_crafty_bay/features/auth/ui/controllers/sign_in_contro
 import 'package:ecommerce_crafty_bay/features/auth/ui/screens/sign_up_screen.dart';
 import 'package:ecommerce_crafty_bay/features/auth/ui/widgets/app_logo.dart';
 import 'package:ecommerce_crafty_bay/features/common/ui/screen/main_bottom_nav_bar_screen.dart';
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -23,7 +24,8 @@ class SignInScreen extends StatefulWidget {
 class _SignInScreenState extends State<SignInScreen> {
   final TextEditingController _emailTEController = TextEditingController();
   final TextEditingController _passwordTEController = TextEditingController();
-
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool obscureText = true;
   final SignInController _signInController = Get.find<SignInController>();
 
   @override
@@ -36,58 +38,75 @@ class _SignInScreenState extends State<SignInScreen> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              const SizedBox(height: 60),
-              const AppLogo(),
-              const SizedBox(height: 24),
-              Text(
-                context.localization.welcomeBack,
-                style: textTheme.titleLarge,
-              ),
-              const SizedBox(height: 8),
-              Text(
-                context.localization.enterYourEmailAndPassword,
-                style: const TextStyle(color: Colors.grey, fontSize: 16),
-              ),
-              const SizedBox(height: 16),
-              TextFormField(
-                controller: _emailTEController,
-                decoration:
-                InputDecoration(hintText: context.localization.email),
-              ),
-              const SizedBox(height: 8),
-              TextFormField(
-                controller: _passwordTEController,
-                obscureText: true,
-                decoration:
-                InputDecoration(hintText: context.localization.password),
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: _onTapSignInButton,
-                child: Text(context.localization.signIn),
-              ),
-              const SizedBox(height: 24),
-              RichText(
-                text: TextSpan(
-                  text: "Don't have an account? ",
-                  style: const TextStyle(
-                      color: Colors.grey, fontWeight: FontWeight.w600),
-                  children: [
-                    TextSpan(
-                        text: 'Sign up',
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: AppColors.themeColor,
-                        ),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = _onTapSignUpButton
-                    ),
-                  ],
+          child: Form(
+            key: _formKey,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            child: Column(
+              children: [
+                const SizedBox(height: 60),
+                const AppLogo(),
+                const SizedBox(height: 24),
+                Text(
+                  context.localization.welcomeBack,
+                  style: textTheme.titleLarge,
                 ),
-              )
-            ],
+                const SizedBox(height: 8),
+                Text(
+                  context.localization.enterYourEmailAndPassword,
+                  style: const TextStyle(color: Colors.grey, fontSize: 16),
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _emailTEController,
+                  decoration:
+                  InputDecoration(hintText: context.localization.email),
+                    validator: (String? value) {
+                      String email = value ?? '';
+                      if (!EmailValidator.validate(email)) {
+                        return 'Enter a valid email';
+                      }
+                      return null;
+                    }
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  controller: _passwordTEController,
+                  obscureText: true,
+                  decoration:
+                  InputDecoration(hintText: context.localization.password),
+                  validator: (String? value) {
+                    if ((value?.isEmpty ?? true) || value!.length < 6) {
+                      return 'Enter a password more than 6 letters';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: _onTapSignInButton,
+                  child: Text(context.localization.signIn),
+                ),
+                const SizedBox(height: 24),
+                RichText(
+                  text: TextSpan(
+                    text: "Don't have an account? ",
+                    style: const TextStyle(
+                        color: Colors.grey, fontWeight: FontWeight.w600),
+                    children: [
+                      TextSpan(
+                          text: 'Sign up',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: AppColors.themeColor,
+                          ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = _onTapSignUpButton
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
@@ -95,7 +114,9 @@ class _SignInScreenState extends State<SignInScreen> {
   }
 
   Future<void> _onTapSignInButton() async {
-    // TODO: validate the form
+    if (!_formKey.currentState!.validate()) { // Validate the form
+      return;
+    }
     SignInRequestModel signInRequestModel = SignInRequestModel(
       email: _emailTEController.text.trim(),
       password: _passwordTEController.text,
